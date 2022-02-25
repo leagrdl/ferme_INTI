97ad156ebce5f7c9188c4c87c8add93841d4d785
MaevaSouchu <maeva.souchou@gmail.com>
2022-02-25 09:55:28 +0100

    Commit du gestionFerme

---
+++ src/main/java/com/inti/GestionFermeApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFermeApplication.class, args);
+	}
+
+}
